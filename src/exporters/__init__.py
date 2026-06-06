25ea7ca913afd7721534b0d6bb61a4957b1bd71c
Hermann samimi <hermann@itsecurity.ee>
2026-06-06 19:31:47 +0200

    Remove Yelp support and related functionality from scrapers and exporters

--- src/exporters/__init__.py
+++ src/exporters/__init__.py
@@ -1,6 +1,5 @@
 from src.exporters.csv_exporter import export_csv
-from src.exporters.discord import send_discord_summary
 from src.exporters.json_exporter import export_json
 from src.exporters.xlsx_exporter import export_xlsx
 
-__all__ = ["export_csv", "export_json", "export_xlsx", "send_discord_summary"]
+__all__ = ["export_csv", "export_json", "export_xlsx"]
--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,9 +1,7 @@
 from src.scrapers.trustpilot import TrustpilotScraper
-from src.scrapers.yelp import YelpScraper
 
 SCRAPER_MAP = {
     "trustpilot": TrustpilotScraper,
-    "yelp": YelpScraper,
 }
 
-__all__ = ["SCRAPER_MAP", "TrustpilotScraper", "YelpScraper"]
+__all__ = ["SCRAPER_MAP", "TrustpilotScraper"]
